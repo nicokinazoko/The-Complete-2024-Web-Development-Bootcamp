a8058d3c2f9ba9d6a4bd7cd512dc2b5f1d7248cf
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-01-24 10:06:06 +0700

    Learn about Javascript Variable Exercise

---
+++ part-14/14.5 Javascript Variable Exercise/app.js
@@ -0,0 +1,15 @@
+function test() {
+  var a = '3';
+  var b = '8';
+
+  /***********Do not change the code above 👆*******/
+  //Write your code on lines 7 - 9:
+  var c = a;
+  a = b;
+  b = a;
+
+  /***********Do not change the code below 👇*******/
+
+  console.log('a is ' + a);
+  console.log('b is ' + b);
+}
