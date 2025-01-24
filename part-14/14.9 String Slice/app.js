372cbd7032d1e74cc0e6b56aea88bf69ab4af169
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-01-24 10:46:10 +0700

    Learn about string slice

---
+++ part-14/14.9 String Slice/app.js
@@ -0,0 +1,7 @@
+// You have writtern 182 characters, you have -24 character left
+// need to cut to 140 characters
+
+var sentence = prompt('Type the sentence');
+var sentenceSliced = sentence.slice(0, 140);
+
+alert('You have written ' + sentenceSliced);
