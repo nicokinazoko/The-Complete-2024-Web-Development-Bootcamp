f3d840cc4043a881a53295a1304b8dfb4a48414c
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-02-11 11:48:52 +0700

    Create love generator using math random

---
+++ 15. Intermediate Javascript/app.js
@@ -0,0 +1,10 @@
+var n = Math.random();
+n = n * 6;
+n = Math.floor(n);
+console.log(n);
+
+prompt('What is your name?');
+prompt('What is your partner name?');
+var percentage = Math.round(Math.random() * 100);
+alert('Your match is ' + percentage + '%');
+console.log(percentage);
