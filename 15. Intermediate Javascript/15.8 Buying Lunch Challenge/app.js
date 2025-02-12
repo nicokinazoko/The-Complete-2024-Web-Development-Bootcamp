5a8094984dcbe908eebc12819224daabf11565dd
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-02-12 11:26:42 +0700

    Learn buying lunch challenge

---
+++ 15. Intermediate Javascript/15.8 Buying Lunch Challenge/app.js
@@ -0,0 +1,12 @@
+function whosPaying(names) {
+  /******Don't change the code above*******/
+  //Write your code here.
+
+  return (
+    names[Math.floor(Math.random() * names.length)] +
+    ' is going to buy lunch today!'
+  );
+  /******Don't change the code below*******/
+}
+
+console.log(whosPaying(['Angela', 'Ben', 'Jenny', 'Michael', 'Chloe']));
