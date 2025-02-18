bde97e55422e832472354ea8d7435103f99d3a3f
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-02-18 10:42:58 +0700

    Dicee Challenge Step 6 - Change the Title to Display a Winner

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -13,3 +13,12 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document
   .querySelector('.img2')
   .setAttribute('src', './images/dice' + randomNumber2 + '.png');
+
+// add condition for random number to set the header
+if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').textContent = '🚩Player 1 Win ';
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector('h1').textContent = 'Player 2 Win 🚩';
+} else {
+  document.querySelector('h1').textContent = 'Draw!';
+}
