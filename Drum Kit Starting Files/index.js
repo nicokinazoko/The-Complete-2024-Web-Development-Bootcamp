c1ddc87fdf7aa6f95f61e4a71f121f7b165f7b24
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-02-19 10:15:34 +0700

    Learn challenge High Order function

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,22 @@
+// start challenge high order function
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function division(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
+
+// end challenge high order function
