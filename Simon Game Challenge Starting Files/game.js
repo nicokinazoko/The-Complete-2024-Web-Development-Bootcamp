9175d4b0f31082cf062afc13eb6badb48f72ab37
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-02-27 10:40:10 +0700

    add animation and flash for button

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -7,3 +7,14 @@ function nextSequence() {
   var randomNumber = Math.round(Math.random(0, 3) * 3);
   return randomNumber;
 }
+
+$('.btn').on('click', function () {
+  var buttonId = $(this).attr('id');
+
+  $('#' + buttonId)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio('./sounds/' + buttonId + '.mp3');
+  audio.play();
+});
