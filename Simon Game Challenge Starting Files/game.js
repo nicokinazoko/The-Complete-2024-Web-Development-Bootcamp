a2196cb857873ee5f871ddf4843c00b6c791a705
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-02-27 11:28:39 +0700

    add validation to check if keyboard is pressed or not, then it will start the the level

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,27 +1,43 @@
+var buttonColours = ['red', 'blue', 'green', 'yellow'];
 var userClickedPattern = [];
 var gamePattern = [];
-var buttonColours = ['red', 'blue', 'green', 'yellow'];
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
 
-function nextSequence() {
-  var randomNumber = Math.round(Math.random(0, 3) * 3);
-  return randomNumber;
-}
+var level = 0;
+var keyboardPressed = false;
+
+var keyboardEvent = $('body').on('keypress', function () {
+  if (!keyboardPressed) {
+    $('#level-title').text('Level ' + level);
+
+    nextSequence();
+    keyboardPressed = true;
+  }
+});
 
 $('.btn').on('click', function () {
   var buttonId = $(this).attr('id');
 
-  animatePress(buttonId);
+  userClickedPattern.push(buttonId);
   $('#' + buttonId)
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio('./sounds/' + buttonId + '.mp3');
-  audio.play();
-
-  userClickedPattern.push(buttonId);
+  playSound(buttonId);
+  animatePress(buttonId);
 });
+
+function nextSequence() {
+  level++;
+  $('#level-title').text('Level ' + level);
+  var randomNumber = Math.round(Math.random(0, 3) * 3);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio('./sounds/' + name + '.mp3');
+  audio.play();
+}
 
 function animatePress(currentColour) {
   var button = $('#' + currentColour);
