c3f262f478eb7ae74847668b7c8d11f0163f1c21
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-03-11 10:42:49 +0700

    Learn about install external modules

---
+++ part-22/22.3 The NPM Package Manager and Installing External Node Modules/index.js
@@ -0,0 +1,11 @@
+// using module
+import generateName from 'sillyname';
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}`);
+
+// challenge
+import { randomSuperhero } from 'superheroes';
+
+const superheroName = randomSuperhero();
+console.log(`I am ${superheroName}!`);
